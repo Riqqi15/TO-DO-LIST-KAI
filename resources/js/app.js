19b139407b0b86b22898ae6960ea6f023126e7e2
Brandon Deandra <brandondeandra05@gmail.com>
2026-08-01 20:32:00 +0700

    chore: establish shadcn frontend foundation

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,9 +5,9 @@ import { createApp, h } from 'vue';
 createInertiaApp({
     title: (title) => `${title} - To Do List KAI`,
     resolve: (name) => {
-        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true });
+        const pages = import.meta.glob('./Pages/**/*.vue');
 
-        return pages[`./Pages/${name}.vue`];
+        return pages[`./Pages/${name}.vue`]();
     },
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
