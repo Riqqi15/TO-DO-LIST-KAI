85f2f5f22f6a3cd98db8c1eb733fe092b3637162
Brandon Deandra <brandondeandra05@gmail.com>
2026-08-20 09:03:17 +0700

    refactor(database): update system categories list
    - Removed system categories: PR, Meeting, Lainnya, Report Progress
    
    - Added new system categories: KAK, RAB, AHS
    
    - Update global DatabaseSeeder to reflect the latest standard categories.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,14 +12,12 @@ class DatabaseSeeder extends Seeder
         DB::table('categories')->insert([
             ['name' => 'NPD ACC', 'slug' => 'npd-acc', 'is_system' => true, 'created_at' => now(), 'updated_at' => now()],
             ['name' => 'G63', 'slug' => 'g63', 'is_system' => true, 'created_at' => now(), 'updated_at' => now()],
-            ['name' => 'PR', 'slug' => 'pr', 'is_system' => true, 'created_at' => now(), 'updated_at' => now()],
             ['name' => 'SPK', 'slug' => 'spk', 'is_system' => true, 'created_at' => now(), 'updated_at' => now()],
             ['name' => 'Laporan Pendahuluan', 'slug' => 'laporan-pendahuluan', 'is_system' => true, 'created_at' => now(), 'updated_at' => now()],
             ['name' => 'Laporan Draft', 'slug' => 'laporan-draft', 'is_system' => true, 'created_at' => now(), 'updated_at' => now()],
             ['name' => 'Laporan Akhir', 'slug' => 'laporan-akhir', 'is_system' => true, 'created_at' => now(), 'updated_at' => now()],
-            ['name' => 'Meeting', 'slug' => 'meeting', 'is_system' => true, 'created_at' => now(), 'updated_at' => now()],
-            ['name' => 'Report Progress', 'slug' => 'report-progress', 'is_system' => true, 'created_at' => now(), 'updated_at' => now()],
-            ['name' => 'Lainnya', 'slug' => 'lainnya', 'is_system' => true, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'KAK', 'slug' => 'kak', 'is_system' => true, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'RAB, AHS', 'slug' => 'rab-ahs', 'is_system' => true, 'created_at' => now(), 'updated_at' => now()],
         ]);
 
         // Buat akun demo melalui form
